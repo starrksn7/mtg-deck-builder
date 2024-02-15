c35d2e28c66a726035006f9f779c945fd7ad5a18
Robert Weeks <rpweeks_2@yahoo.com>
2024-02-14 19:52:54 -0500

    added DeckDao

---
+++ src/main/java/com/builder/deckbuilder/dao/DeckDao.java
@@ -0,0 +1,20 @@
+package com.builder.deckbuilder.dao;
+
+import com.builder.deckbuilder.model.Deck;
+import com.builder.deckbuilder.model.Card;
+import java.util.List;
+
+public interface DeckDao {
+
+    List<Deck> findDecksByUser(String username);
+
+    Deck getDeckByName(String name);
+
+    Deck getDeckById(int id);
+
+    Boolean addCardToDeck(Card card);
+
+    Boolean removeCardFromDeck(Card card);
+
+    Boolean updateDeck(int id, String deckName, String commander);
+}
