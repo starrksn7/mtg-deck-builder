87bc314c2f5eb3f8a7a7bdbe6f403319a6360747
Robert Weeks <rpweeks_2@yahoo.com>
2024-04-23 19:19:54 -0400

    added function to card controller.

--- src/main/java/com/builder/deckbuilder/controller/CardController.java
+++ src/main/java/com/builder/deckbuilder/controller/CardController.java
@@ -1,13 +1,24 @@
 package com.builder.deckbuilder.controller;
 
+import com.builder.deckbuilder.dao.CardDao;
+import com.builder.deckbuilder.model.Card;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/card")
 @CrossOrigin
 public class CardController {
+    private CardDao cardDao;
 
-
+    private CardController(CardDao cardDao){
+        this.cardDao = cardDao;
+    }
+    public List<Card> searchForCardByName(String name) throws UnsupportedEncodingException {
+        return cardDao.searchForCardByName(name);
+    }
 }
