75e559b7b1ebe1eca91f8508b989cd7ed32f4fc8
Robert Weeks <rpweeks_2@yahoo.com>
2024-04-27 13:33:12 -0400

    progressing on deck controller.

--- src/main/java/com/builder/deckbuilder/controller/CardController.java
+++ src/main/java/com/builder/deckbuilder/controller/CardController.java
@@ -3,6 +3,7 @@ package com.builder.deckbuilder.controller;
 import com.builder.deckbuilder.dao.CardDao;
 import com.builder.deckbuilder.model.Card;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class CardController {
     private CardController(CardDao cardDao){
         this.cardDao = cardDao;
     }
+    @GetMapping(path="")
     public List<Card> searchForCardByName(String name) throws UnsupportedEncodingException {
         return cardDao.searchForCardByName(name);
     }
--- src/main/java/com/builder/deckbuilder/controller/DeckController.java
+++ src/main/java/com/builder/deckbuilder/controller/DeckController.java
@@ -1,9 +1,13 @@
 package com.builder.deckbuilder.controller;
 
 import com.builder.deckbuilder.dao.DeckDao;
+import com.builder.deckbuilder.model.Deck;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/decks")
@@ -13,5 +17,10 @@ public class DeckController {
 
     private DeckController(DeckDao deckDao){
         this.deckDao = deckDao;
+    }
+
+    @GetMapping(path = "")
+    public List<Deck> findDecksByUser(String username){
+        return deckDao.findDecksByUser(username);
     }
 }
