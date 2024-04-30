9d6ab42c7b7195417d2ed9c31903ef5695ee6873
Robert Weeks <rpweeks_2@yahoo.com>
2024-04-30 19:28:57 -0400

    continued working on deckController

--- src/main/java/com/builder/deckbuilder/controller/DeckController.java
+++ src/main/java/com/builder/deckbuilder/controller/DeckController.java
@@ -2,10 +2,7 @@ package com.builder.deckbuilder.controller;
 
 import com.builder.deckbuilder.dao.DeckDao;
 import com.builder.deckbuilder.model.Deck;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,10 @@ public class DeckController {
     @GetMapping(path = "")
     public List<Deck> findDecksByUser(String username){
         return deckDao.findDecksByUser(username);
+    }
+
+    @PostMapping(path="/create")
+    public boolean createDeck(int userId, String deckName, String commander){
+        return deckDao.createDeck(userId, deckName, commander);
     }
 }
