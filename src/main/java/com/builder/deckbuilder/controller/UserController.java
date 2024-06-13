cb39ea2546c9eea823ba2125eed2adb59bd8bbee
Robert Weeks <rpweeks_2@yahoo.com>
2024-06-13 19:21:26 -0400

    added updateUserProfile to user controller

--- src/main/java/com/builder/deckbuilder/controller/UserController.java
+++ src/main/java/com/builder/deckbuilder/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.builder.deckbuilder.controller;
+
+import com.builder.deckbuilder.dao.UserDao;
+import com.builder.deckbuilder.model.User;
+import com.builder.deckbuilder.model.UserDTO;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user")
 @CrossOrigin
 public class UserController {
+
+    private UserDao userDao;
+    @PutMapping(path="/update")
+    public User updateUserProfile(int userId, UserDTO updatedUser){
+        return userDao.updateUserProfile(userId, updatedUser);
+    }
 }
