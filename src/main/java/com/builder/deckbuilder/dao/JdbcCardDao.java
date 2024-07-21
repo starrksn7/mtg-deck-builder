d35b188766666a65339de0c31053fa4727bb15e8
Robert Weeks <rpweeks_2@yahoo.com>
2024-07-21 14:40:52 -0400

    receiving a valid resonse from scryfall.  Need to parse it to a card object.

--- src/main/java/com/builder/deckbuilder/dao/JdbcCardDao.java
+++ src/main/java/com/builder/deckbuilder/dao/JdbcCardDao.java
@@ -1,19 +1,17 @@
 package com.builder.deckbuilder.dao;
 
 import com.builder.deckbuilder.model.Card;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 @Component
 public class JdbcCardDao implements CardDao{
@@ -24,11 +22,8 @@ public class JdbcCardDao implements CardDao{
     }
     String scryfallUrl = "https://api.scryfall.com";
     public List<Card> searchForCardByName(String name) throws UnsupportedEncodingException {
-            System.out.println(1);
-            System.out.println(name);
             String encodedName = URLEncoder.encode(name, "UTF-8");
             String uri = scryfallUrl + "/cards/search?unique=prints&q=" + encodedName;
-        System.out.println(2);
         try {
             return getCardsFromUri(uri);
         } catch (IOException e) {
@@ -37,14 +32,28 @@ public class JdbcCardDao implements CardDao{
     }
 
     public List<Card> getCardsFromUri(String uri) throws IOException {
-        System.out.println(3);
-        URL url = new URL(uri);
-        URLConnection connection = url.openConnection();
-        System.out.println(4);
-        InputStreamReader reader = new InputStreamReader(connection.getInputStream());
-        Type listType = new TypeToken<List<Card>>(){}.getType();
-        System.out.println(5);
-        return new Gson().fromJson(reader, listType);
+        List<Card> list = new ArrayList<>();
+
+        URL expansionsListUrl = new URL(uri);
+        HttpURLConnection conn = (HttpURLConnection) expansionsListUrl.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        int responsecode = conn.getResponseCode();
+        if (responsecode != 200) {
+            throw new RuntimeException("HttpResponseCode: " + responsecode);
+        }
+
+        StringBuilder body = new StringBuilder();
+        Scanner scanner = new Scanner(expansionsListUrl.openStream());
+
+        while (scanner.hasNext()) {
+            body.append(scanner.nextLine());
+        }
+        scanner.close();
+//        return String.valueOf(body);
+        System.out.println(String.valueOf(body));
+        return list;
     }
 
 }
